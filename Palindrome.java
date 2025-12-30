c5592409360978d85c1c2844e62529e3df153081
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-12-30 21:33:05 +0200

    fix pali

--- Palindrome.java
+++ Palindrome.java
@@ -7,6 +7,7 @@ public class Palindrome {
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
+		if (s.length() == 0) return false;
 		if (s.length() == 1) return true;
 		if (s.charAt(0) == s.charAt(s.length()-1)) {
 			return isPalindrome(s.substring(1,s.length()-1));
