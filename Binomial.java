5354c546fce0ad5247da77adaf5a73922025751e
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-12-30 21:31:47 +0200

    optimuized

--- Binomial.java
+++ Binomial.java
@@ -7,7 +7,7 @@ public class Binomial {
     	// System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
 
 		// Testing the optimized binomial implementation:
-		// System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+		System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
 	}
 
 	// Computes the Binomial function, basic version.
@@ -23,7 +23,13 @@ public class Binomial {
 		//// and then initializes all its elements to -1.
 		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
 		//// Replace the following statement with your code.
-		return 0;
+		int[][] memo = new int[Math.max(n,k)+1][Math.max(n,k)+1];
+		for (int i = 0; i < memo.length; i ++) {
+			for (int j = 0; j < memo[0].length; j ++) {
+				memo[i][j] = -1;
+			}
+		}
+		return binomial(n, k, memo);
 	}
 
 	private static int binomial(int n, int k, int[][] memo) {
